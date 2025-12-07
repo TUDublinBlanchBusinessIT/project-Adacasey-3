8926b23ec6f9d5e85c0e80d25e083ba30301bb81
Adacasey <159260686+Adacasey@users.noreply.github.com>
2025-12-07 23:33:39 +0000

    session start function added

--- login_register.php
+++ login_register.php
@@ -3,11 +3,11 @@
 session_start();
 require_once 'config.php';
 
-if (isset($_POST[''])) {
-    $name = $_POST[''];
-    $email = $_POST[''];
-    $password = $_POST[''];
-    $role = $_POST[''];
+if (isset($_POST['register'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $role = $_POST['role'];
 
     $checkEmail = $conn->query("SELECT email FROM users WHERE email = '$email'");
     if ($checkEmail->num_rows >0) {
@@ -19,6 +19,32 @@ if (isset($_POST[''])) {
 
     header("Location: index.php");
     exit();
+}
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+
+            if ($user['role'] === 'admin') {
+                header("Location: admin_page.php");
+            } else {
+                header("Location: user_page.php");
+            }
+            exit();
+        }
+    }
+
+$_SESSION['login_error'] = 'Incorrect email or password';
+$_SESSION['active_form'] = 'login';
+header("Location: index.php");
+exit();
 }
 
 ?>
